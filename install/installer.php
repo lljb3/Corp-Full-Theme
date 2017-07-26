23744142de45f869a8b53608d7b2ed2b93eb697a
James Burrell <james@kakemultimedia.com>
2017-07-26 09:35:09 -0500

    Makes 6.5.5 Layerslider required

--- install/installer.php
+++ install/installer.php
@@ -46,7 +46,7 @@
 				'slug'               => 'LayerSlider', // The plugin slug (typically the folder name).
 				'source'             => get_template_directory_uri() . '/install/installer/plugins/layerslider.zip', // The plugin source.
 				'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-				'version'            => '6.0', // E.g. 1.0.0. If set, the active plugin must be this version or higher.
+				'version'            => '6.5.5', // E.g. 1.0.0. If set, the active plugin must be this version or higher.
 				'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
 				'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
 				'external_url'       => '', // If set, overrides default API URL and points to an external URL.
