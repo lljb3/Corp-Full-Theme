c619fc69c10131509eff60bba9f140b2ddb167a7
lljb3 <lljb3@lljb3.com>
2017-11-03 21:53:49 -0500

    Corrects error on scripts if Jetpack modules can't be enabled

--- includes/scripts.php
+++ includes/scripts.php
@@ -8,7 +8,11 @@
 	function starkers_script_enqueuer() {
 		/* Theme JS */
 		$app_base = get_template_directory_uri() . '/assets/js';
-		wp_enqueue_script( 'requirejs',  $app_base . '/require.js', array('jquery','backbone','jetpack-carousel'), false, true );
+		if ( class_exists( 'Jetpack' ) && Jetpack::is_module_active( 'photon' ) ) {
+			wp_enqueue_script( 'requirejs',  $app_base . '/require.js', array('jquery','backbone','jetpack-carousel'), false, true );
+		} else {
+			wp_enqueue_script( 'requirejs',  $app_base . '/require.js', array('jquery'), false, true );
+		}
 		wp_localize_script( 'requirejs', 'require', array(
 			'baseUrl' => $app_base,
 			'deps' => array( $app_base . '/site.js')
