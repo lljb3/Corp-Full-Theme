6c09b921ded5906bd22ed2871fc10edb5487883a
lljb3 <lljb3@lljb3.com>
2018-02-11 01:13:21 -0600

    Fixed a broken link for chat

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -82,7 +82,7 @@
                         <ul class="navbar-nav nav">
                             <?php if( !empty( $chat ) ) { 
                                 echo '<script type="text/javascript">' . $chat . '</script>';
-                                echo '<li class="menu-chat menu-item"><a title="Chat" onclick="' . $chat_click . '"><i class="fa fa-comments-o" aria-hidden="true"></a></i></li>'; 
+                                echo '<li class="menu-chat menu-item"><a title="Chat" onclick="' . $chat_click . '"><i class="fa fa-comments-o" aria-hidden="true"></i></a></li>'; 
                             } ?>
                             <?php if( !empty( $phone ) ) { 
                                 echo '<li class="menu-phone menu-item"><a title="Call" href="tel:' . $phone . '"><i class="fa fa-mobile" aria-hidden="true" ></i></a></li>'; 
