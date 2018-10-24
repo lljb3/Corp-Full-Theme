a6684501c40d535b27f61ba79e3879a6b0467975
James Burrell <bellboisome@Jamess-MacBook-Air.local>
2018-10-24 10:55:57 -0500

    Added WP Smush Pro & WP Rocket 3

--- install/installer.php
+++ install/installer.php
@@ -14,13 +14,6 @@
 				'slug'      => 'fusion-extension-image',
 				'required'  => true,
 			),
-			/*
-			array(
-				'name'      => 'Bootstrap 3 Shortcodes',
-				'slug'      => 'bootstrap-3-shortcodes',
-				'required'  => false,
-			),
-			*/
 			array(
 				'name'      => 'Responsive Lightbox & Gallery',
 				'slug'      => 'responsive-lightbox',
@@ -77,9 +70,14 @@
 				'required'  => true,
 			),
 			array(
-				'name'      => 'WP Smush',
-				'slug'      => 'wp-smushit',
-				'required'  => true,
+				'name'               => 'WP Smush Pro', // The plugin name.
+				'slug'               => 'wp-smush-pro', // The plugin slug (typically the folder name).
+				'source'             => get_template_directory_uri() . '/install/installer/plugins/wpsmushpro.zip', // The plugin source.
+				'required'           => true, // If false, the plugin is only 'recommended' instead of required.
+				'version'            => '2.9.0', // E.g. 1.0.0. If set, the active plugin must be this version or higher.
+				'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
+				'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
+				'external_url'       => '', // If set, overrides default API URL and points to an external URL.
 			),
 			array(
 				'name'      => 'Yoast SEO',
